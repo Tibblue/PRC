f43f63c1b06e286aeda93b74969f89a7e9c967f4
Kiko <fjmo_1997@hotmail.com>
2019-06-13 15:11:26 +0100

    projeto_v3
    -added user profiles
      -contains favorite animes
      -simple proof of concept login with username
      -using json-server as database for the user list
    -a few pages got design changes to improve look
    -new Home page with top5 best animes
    -a few more small things

--- projeto/frontend/src/App.vue
+++ projeto/frontend/src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-toolbar dark>
       <v-toolbar-items>
-        <v-btn flat @click="goToHome()"><h2>Home</h2></v-btn>
+        <v-btn color="indigo darken-2" @click="goToHome()"><h2>Home</h2></v-btn>
         <v-btn flat @click="goToTable('animes')"><h3>Anime</h3></v-btn>
         <v-btn flat @click="goToTable('producers')"><h3>Producer</h3></v-btn>
         <v-btn flat @click="goToTable('studios')"><h3>Studio</h3></v-btn>
       </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn color="indigo darken-2"
+          v-if="!this.$session.has('id')"
+          @click="login('kiko')"
+        ><h2>Login</h2></v-btn>
+        <v-btn flat
+          v-if="this.$session.has('id')"
+          @click="logout()"
+        ><h2>Logout</h2></v-btn>
+        <v-btn color="indigo darken-2"
+          v-if="this.$session.has('id')"
+          @click="goToProfile()"
+        >
+          <h2>{{this.$session.get('id')}} Profile</h2>
+        </v-btn>
+      </v-toolbar-items>
+        <!-- <v-flex v-if="this.$session.has('id')"> -->
+          <v-img
+            :src="this.$session.get('img')"
+            aspect-ratio=1
+            max-width=64
+            max-height=64
+          ></v-img>
+        <!-- </v-flex> -->
     </v-toolbar>
-
 
     <!-- <v-container> -->
     <!-- <v-flex ma-5> -->
@@ -21,9 +45,7 @@
         <v-btn icon v-for="icon in icons" :key="icon[0]">
           <v-icon size="24px" @click="goTo(icon[1])">{{icon[0]}}</v-icon>
         </v-btn>
-
         <v-divider></v-divider>
-
         <v-card-text>
           2019 — <strong>Francisco Oliveira</strong> —
           Projeto de PRC (Perfil PLC), Universidade do Minho
@@ -54,8 +76,17 @@
         this.$router.push('/')
       },
       goToTable: function (table) {
-        // this.$emit('filmeSelected', item)
         this.$router.push('/'+table)
+      },
+      login: function () {
+        this.$router.push('/login')
+      },
+      logout: function () {
+        this.$session.clear()
+        this.$router.go(0)
+      },
+      goToProfile: function () {
+        this.$router.push('/profile')
       }
     }
   }
